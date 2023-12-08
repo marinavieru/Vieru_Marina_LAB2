7fb6e7b7c3a2029fa019891f8423d1ea33fc23e4
MARINA-MIHAELA VIERU <marina.vieru@stud.ubbcluj.ro>
2023-12-08 13:39:06 +0000

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vieru_Marina_LAB2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vieru_Marina_LAB2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        [Display(Name = "Publisher Name")]
+        public string PublisherName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
